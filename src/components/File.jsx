2bf0fec791ab8df47715f33dc9523886ff5f48b8
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-15 11:02:40 +0000

    15 Apr 2023, 16:32

--- src/components/File.jsx
+++ src/components/File.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
-const File = () => {
-  return (
-    <div>File</div>
-  )
+import file from './file.png'
+const File = ({data}) => {
+  return(
+    <div style={{display: "flex" }}>
+      <img  id={data.id}  src ={file} alt="folder" width={50} height={50}/>
+      <div style={{paddingTop:"1rem"}} id={data.id}>{data.name}</div>
+      </div> 
+    )
 }
 
 export default File
