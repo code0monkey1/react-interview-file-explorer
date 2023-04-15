7c29874107029750d23a19516f3bf3954d079521
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-15 11:18:07 +0000

    15 Apr 2023, 16:48

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -2,17 +2,34 @@ import image from './folder.png';
 
 const Folder = ({data,toggleOpen}) => {
   
+  const styles={
+
+  container: {
+   display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  width: "100%",
+  maxWidth: "600px",
+  margin: "0 auto"
+}
+,
+buttonContainer: {
+  display: "flex",
+  justifyContent: "flex-end",
+  alignItems: "center"
+}
+  }
   
    const copy=JSON.parse(JSON.stringify(data));
   console.log("copy",copy)
   return (<>
-   <div style={{display: "flex" ,"justifyContent": "space-between","alignItems":"center"}}>
-    <div style={{display: "flex" }}>
+   <div style={styles.container}>
+    <div style={styles.buttonContainer}>
       <img  id={data.id} onClick={()=>{toggleOpen(data.id)}}  src ={image} alt="folder" width={50} height={50}/>
       <div style={{paddingTop:"1rem"}} id={data.id}>{data.name}</div>
       </div> 
     </div>
-    <div style={{display: "flex"  }}>
+    <div style={styles.buttonContainer}>
       <button>Add File</button>
       <button style={{marginLeft:"0.5rem"}}>Add Folder</button>
     </div>
