964c002b69cad65b1471b5639c446eae790830cd
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-15 11:19:26 +0000

    15 Apr 2023, 16:49

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -2,36 +2,30 @@ import image from './folder.png';
 
 const Folder = ({data,toggleOpen}) => {
   
-  const styles={
-
+const styles = {
   container: {
-   display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-  width: "100%",
-  maxWidth: "600px",
-  margin: "0 auto"
-}
-,
-buttonContainer: {
-  display: "flex",
-  justifyContent: "flex-end",
-  alignItems: "center"
-}
-  }
+    display: 'flex',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    marginLeft: '1rem',
+  },
+};
   
    const copy=JSON.parse(JSON.stringify(data));
   console.log("copy",copy)
   return (<>
-   <div style={styles.container}>
-    <div style={styles.buttonContainer}>
-      <img  id={data.id} onClick={()=>{toggleOpen(data.id)}}  src ={image} alt="folder" width={50} height={50}/>
-      <div style={{paddingTop:"1rem"}} id={data.id}>{data.name}</div>
-      </div> 
-    </div>
-    <div style={styles.buttonContainer}>
-      <button>Add File</button>
-      <button style={{marginLeft:"0.5rem"}}>Add Folder</button>
+ <div style={styles.container}>
+      <div style={styles.buttonContainer}>
+        <img id={data.id} onClick={() => toggleOpen(data.id)} src={image} alt="folder" width={50} height={50} />
+        <div style={{ paddingTop: '1rem' }} id={data.id}>{data.name}</div>
+      </div>
+      <div style={styles.buttonContainer}>
+        <button>Add File</button>
+        <button style={{ marginLeft: '0.5rem' }}>Add Folder</button>
+      </div>
     </div>
     </>
   )
