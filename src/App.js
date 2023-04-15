f0358aa0f632a75026c52a201c8ebcff68af7f6f
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-15 13:46:16 +0000

    15 Apr 2023, 19:16

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import { useState } from 'react';
+
 import './App.css';
 import File from './components/File.jsx';
 import Folder from './components/Folder.jsx';
 import treeData from './data/folderData';
 import useTree from './hooks/useTree';
-import getToggledTree from './utils/toggleOpen';
 
 function App() {
 
-  // const [data,setData]=useState(treeData)
-           const{tree:data,getToggledTree,setTree} =  useTree(treeData)
+    const{tree:data,getToggledTree,setTree} =  useTree(treeData)
 
     const getRenderedTree =(parent,marginLeft=1)=>{
       
       const newMargin=marginLeft+1
-      return ( <div key={parent && parent.id}  style={{marginLeft:`${newMargin}rem`}}>
-        { parent && parent.isFolder? <> 
+      return parent && ( <div key={parent.id}  style={{marginLeft:`${newMargin}rem`}}>
+        {parent.isFolder? <> 
               <Folder  data={parent}/>
               {
                 parent.open?parent.items.map(item=> getRenderedTree(item)):''
@@ -25,7 +23,6 @@ function App() {
                <File data={parent}/> 
         }
       </div>)
-
     }
   
   const toggler=(event,data)=>{
@@ -34,7 +31,7 @@ function App() {
 
       setTree(newTree)
   }
-   console.log("The tree data is: " + JSON.stringify(data,null,2)) 
+
   return (
     <div onClick={(event)=>toggler(event,data)} style={{margin:"4rem"}}>
       {
