c7f80ba7b413da352a2f19a071497d42d8ac1caa
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-15 10:37:01 +0000

    15 Apr 2023, 16:07

--- src/App.js
+++ src/App.js
@@ -2,82 +2,11 @@ import { useState } from 'react';
 import './App.css';
 import File from './components/File.jsx';
 import Folder from './components/Folder.jsx';
+import foldersData from './data/folderData';
 
 function App() {
 
-  const [data,setData]=useState({
-          id:"10",
-          name: "root",
-          isFolder: true,
-          open:false,
-          items: [
-            {
-              id:"2",
-              name: "public",
-              isFolder: true,
-              open:false,
-              items: [
-                {
-                  id:"3",
-                  name: "public nested 1",
-                  isFolder: true,
-                  items: [
-                    {
-                      id:"4",
-                      name: "index.html",
-                      isFolder: false,
-                      items: []
-                    },
-                    {
-                      id:"5",
-                      name: "hello.html",
-                      isFolder: false,
-                      items: []
-                    }
-                  ]
-                },
-                {
-                  id:"6",
-                  name: "public_nested_file",
-                  isFolder: false,
-                  items: []
-                }
-              ]
-            },
-            {
-              id:"7",
-              name: "src",
-              isFolder: true,
-              items: [
-                {
-                  id:"8",
-                  name: "App.js",
-                  isFolder: false,
-                  items: []
-                },
-                {
-                  id:"9",
-                  name: "Index.js",
-                  isFolder: false,
-                  items: []
-                },
-                {
-                  id:"10",
-                  name: "styles.css",
-                  isFolder: true,
-                  open: false,
-                  items: []
-                }
-              ]
-            },
-            {
-              id:"11",
-              name: "package.json",
-              isFolder: false,
-              items: []
-            }
-          ]
-        })
+  const [data,setData]=useState(foldersData)
 
           
   const toggleOpen=(id)=>{
