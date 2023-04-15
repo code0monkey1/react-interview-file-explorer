68c999e1eac8a99984470c6c389917cc7fd7b966
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-15 13:04:41 +0000

    15 Apr 2023, 18:34

--- src/hooks/useTree.js
+++ src/hooks/useTree.js
@@ -1,14 +1,57 @@
 
-import { useState } from "react"
+import { useEffect, useState } from "react";
 
 const useTree=(treeData)=>{
 
      const [tree,setTree]=useState()
-    
+
+     useEffect(() => {
      
+       setTree(treeData)
+    }, [])
 
+const getToggledTree=(event,data)=>{
+        
+          const head=JSON.parse(JSON.stringify(data));
 
+          const id=event.target.id
 
+          console.log("The id obtained is",event.target.id);
+            
+         
+          toggleItem(head,id)
 
+          return head;
+  }
+
+function toggleItem(head,id){
+             
+            if(head.isFolder && head.id===id) {
+                 head.open=!head.open;
+              
+                 closeItems(head.items)            
+            }               
+                          
+             for (let item of head.items)
+                  toggleItem(item,id)      
+          }
    
+const closeItems =(items)=>{  
+  
+      items.open=false;
+
+      items.forEach(subItem=>{
+               if(subItem.isFolder){
+                  subItem.open=false;
+                  closeItems(subItem.items)
+              }
+      })
+      
+ }
+
+ return{
+  getToggledTree,
+  tree
+ }
+
 }
