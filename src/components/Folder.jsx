bd211fa9e6322742258cea414bf882d7a11c106b
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-15 11:13:09 +0000

    15 Apr 2023, 16:43

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -6,7 +6,7 @@ const Folder = ({data,toggleOpen}) => {
    const copy=JSON.parse(JSON.stringify(data));
   console.log("copy",copy)
   return (<>
-   <div style={{display: "flex" ,"justifyContent": "space-between"}}>
+   <div style={{display: "flex" ,"justifyContent": "space-between","alignItems":"center"}}>
     <div style={{display: "flex" }}>
       <img  id={data.id} onClick={()=>{toggleOpen(data.id)}}  src ={image} alt="folder" width={50} height={50}/>
       <div style={{paddingTop:"1rem"}} id={data.id}>{data.name}</div>
