15cb544bdb7009ab5f0a722417e9aef5669019ee
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-15 16:16:40 +0000

    15 Apr 2023, 21:46

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import image from './folder.png';
 const Folder = ({data}) => {
   
-  const[add,setAdd]=useState(false)
+  const[fileType,setFileType]=useState(null)
 
 const styles = {
   container: {
@@ -32,12 +32,12 @@ const styles = {
       </div>
      
       <div style={styles.buttonContainer}>
-        <button>Add File</button>
-        <button style={{ marginLeft: '0.5rem' }}>Add Folder</button>
+        <button onClick={()=>setFileType('file')}>Add File</button>
+        <button onClick={()=>setFileType('folder')} style={{ marginLeft: '0.5rem' }}>Add Folder</button>
       </div>
     </div>
     <br/>
-     <form style={styles.form}>
+     <form style={{marginLeft:"1rem"}}>
              <div>Whatever</div>
       </form>
     </>
