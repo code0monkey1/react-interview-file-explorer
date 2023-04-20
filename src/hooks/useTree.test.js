f95eaa8d8a2b4cd417a4188838bd77febe256d59
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-20 06:02:00 +0000

    20 Apr 2023, 11:32

--- src/hooks/useTree.test.js
+++ src/hooks/useTree.test.js
@@ -1,12 +1,23 @@
 import { renderHook } from '@testing-library/react';
-import { useTree } from './useTree';
+import useTree from './useTree';
 
 describe('first',()=>{
     
 
     it('first-test',()=>{
       const {result} = renderHook(useTree)
-      console.log(result.current)
+      expect(result.current.tree).toEqual(undefined)
+    })
+
+    it('gets the json object',()=>{
+        const myJson={
+          file:"whatever",
+          folder:"whatever"
+        }
+
+        const {result} = renderHook(useTree)
+
+        expect(result.current.tree).toBe(undefined)
     })
     
 })
