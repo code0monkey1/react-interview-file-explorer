ba8367f9c06a8c64281f3ec9200371d710e9012c
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-20 05:53:07 +0000

    20 Apr 2023, 11:23

---
+++ src/hooks/useTree.test.js
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+// import { useTree } from './useTree';
+
+
+describe('useTree', () => {
+
+    if('passes the test',()=>{
+      expect(true).toBe(true);
+    })
+})
