852c6674e94bac0b5afa0730432548b5f35e1110
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-23 04:26:10 +0000

    23 Apr 2023, 09:56

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -1,19 +1,4 @@
 import { useState } from 'react';
-import data from '../data/folderData';
-import file from './file.png';
-import folder from './folder.png';
-
-const styles = {
-  container: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  buttonContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    marginLeft: '1rem',
-  },
-};
 
 const Folder = ({data}) => {
    const [open,setOpen]=useState(false)
