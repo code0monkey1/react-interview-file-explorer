674e195d88d1c23789df3df83b572eea116ce274
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-23 04:00:35 +0000

    23 Apr 2023, 09:30

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -16,33 +16,11 @@ const styles = {
 
 const Folder = ({data,inputRef}) => {
    
-  const[type,setType] = useState(null)
-  const[toggle,setToggle] = useState(false)
-
-
-  return (
-  <>
-    <div  
-      style={styles.container} 
-     >
-      
-      <div style={styles.buttonContainer}>
-        <img id={data.id} src={folder} alt="folder" width={60} height={60} />
-        <div style={{ paddingTop: '1rem' }} >{data.name}</div>
-      </div>
-     
-      <div style={styles.buttonContainer}>
-        <button onClick={()=>{setType('file');setToggle(!toggle)}}  name='file'>Add File</button>
-        <button onClick={()=>{setType('folder');setToggle(!toggle)}} name='folder'  style={{ marginLeft: '0.5rem' }}>Add Folder</button>
-      </div>
-    </div>
-    <br/>
-     <div style={{marginLeft:"5rem",display:(toggle?'':"none")}}>
-             <img src={type==="file"?file:folder} alt="folder" width={60} height={60} />
-             <input ref={inputRef} data-id={data.id} data-type={type} type='text' placeholder={`name of the ${type}`}></input>
-      </div>
+  return<>
+  {data.name}
+  
   </>
-  )
+  
 }
 
 export default Folder
