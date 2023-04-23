fdbf505949327dbb56fb73dcc98c17eb310ebdd9
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-23 11:06:43 +0000

    23 Apr 2023, 16:36

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import useTree from './hooks/useTree';
 function App() {
 
     const{tree,addNewNode,removeNode} =  useTree(treeData)
-  
+   console.log("new tree: " + JSON.stringify(tree,null,2))
   return (
        <div >
          {tree && <Folder addNewNode={addNewNode} removeNode={removeNode}  data={tree} />}
