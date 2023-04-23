dc34e81d3812bfd88dbb2fb7dd252344750ffb73
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-23 04:36:40 +0000

    23 Apr 2023, 10:06

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -2,17 +2,27 @@ import { useState } from 'react';
 
 const Folder = ({data}) => {
    const [open,setOpen]=useState(false)
+
   const styles={
     foldersStyle:{
       paddingLeft:"2rem",
       display:open?"none":"block" 
+    },
+    itemStyle:{
+        display:"flex",
+        justifyContent:"space-between",
     }
+
   }
    return<>
 
-      <div onClick={()=>{setOpen(!open)}}>
+      <div onClick={()=>{setOpen(!open)}} style={{width:"20rem",backgroundColor:"grey"}} >
         <span>{data.isFolder?<>🗂️</>:<>📄</>}</span>
         {data.name}
+    
+          <span>Add File</span>
+          <span>Add Folder</span>
+    
       </div> 
 
       <div  style={styles.foldersStyle}>
