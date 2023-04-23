f8811000c189f4f1be4c9b4bf42e629116c81343
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-23 06:18:45 +0000

    23 Apr 2023, 11:48

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -2,6 +2,11 @@ import { useState } from 'react';
 
 const Folder = ({data}) => {
      const [open,setOpen]=useState(false)
+     
+const [showInput,setShowInput]=useState({
+  visible: false,
+  isFolder:false
+})
 
   const styles={
     foldersStyle:{
@@ -17,8 +22,23 @@ const Folder = ({data}) => {
    return<>
      <div 
       onClick={()=>{setOpen(!open)}}  >    
-        <Item data={data} open={open}/>
-     </div>
+      <div >
+          <div style={{width:"20rem",display:"flex",justifyContent:"space-between"}}>
+            <span>{data.isFolder?<>🗂️</>:<>📄</>}{data.name}</span>
+            <div onClick={(e)=>{setShowInput({...showInput,visible:!showInput.visible})}} style={{display:data.isFolder?"block":"none"}}>
+                <button  onClick={()=>{setShowInput({...showInput,isFolder:false})}}>Add File +</button>
+                <button  onClick={()=>{setShowInput({...showInput,isFolder:true})}}>Add Folder +</button>
+            </div>  
+          </div>
+        { showInput.visible && <div style={{paddingLeft:"2rem"}}>
+            <span>{showInput.isFolder?<>🗂️</>:<>📄</>}
+              <input type="text"/>
+            </span> 
+          </div>
+      }
+
+    </div>
+    </div>
      
       <div style={styles.foldersStyle}>
         {data.items.map((item) =><Folder key={item.id} data={item} />) }
@@ -30,27 +50,8 @@ const Folder = ({data}) => {
 export default Folder
 
 
-const Item=({data})=>{
-const [showInput,setShowInput]=useState({
-  visible: false,
-  isFolder:false
-})
+// const Item=({data})=>{
 
-console.log(JSON.stringify(showInput,null,2))
- return <div >
-     <div style={{width:"20rem",display:"flex",justifyContent:"space-between"}}>
-        <span>{data.isFolder?<>🗂️</>:<>📄</>}{data.name}</span>
-        <div onClick={(e)=>{setShowInput({...showInput,visible:!showInput.visible})}} style={{display:data.isFolder?"block":"none"}}>
-            <button  onClick={()=>{setShowInput({...showInput,isFolder:false})}}>Add File +</button>
-            <button  onClick={()=>{setShowInput({...showInput,isFolder:true})}}>Add Folder +</button>
-        </div>  
-      </div>
-     { showInput.visible && <div style={{paddingLeft:"2rem"}}>
-         <span>{showInput.isFolder?<>🗂️</>:<>📄</>}
-          <input type="text"/>
-         </span> 
-      </div>
-   }
-
-    </div>
-}
+// console.log(JSON.stringify(showInput,null,2))
+//  return 
+// }
