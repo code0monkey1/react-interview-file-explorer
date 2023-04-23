0f410d02d0e103e3a43b9d2e0651cc67f0a1c3ad
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-23 07:30:33 +0000

    23 Apr 2023, 13:00

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 
-import { useEffect, useRef } from 'react';
-import { v4 as uuid } from 'uuid';
 import './App.css';
-import File from './components/File.jsx';
 import Folder from './components/Folder.jsx';
 import treeData from './data/folderData';
 import useTree from './hooks/useTree';
@@ -10,7 +7,7 @@ import useTree from './hooks/useTree';
 function App() {
 
     const{tree:data,addNewNode} =  useTree(treeData)
-  
+    
   return (
        <div >
           <Folder addNode={addNewNode}  data={data} />
--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -2,7 +2,7 @@ import { useRef, useState } from 'react';
 import { v4 as uuid } from 'uuid';
 const Folder = ({data,addNewNode}) => {
 
-     console.log("Data is ",JSON.stringify(data,null,2));
+  
     const [open,setOpen]=useState(false)
     const [showInput,setShowInput]=useState({
       visible: false,
@@ -44,7 +44,8 @@ const Folder = ({data,addNewNode}) => {
        }
         addNewNode(id+"",newNode)
     }
-
+   
+  
 
   }
    return<>
