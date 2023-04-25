6abd26ba79cf010db07fa4d0b0d06bfb0dc8753f
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-25 04:40:53 +0000

    25 Apr 2023, 10:10

---
+++ src/hooks/helpers/index.js
@@ -0,0 +1,32 @@
+import { getDeepCopy } from "../utils/copy";
+
+
+let originalTree ={
+          id:"1",
+          name: "root",
+          isFolder: true,
+          items: [{
+
+          id:"2",
+          name: "root",
+          isFolder: true,
+          items:[]
+          }
+          ,{
+          id:"3",
+          name: "root",
+          isFolder: true,
+           items:[]
+          }
+        ]
+        }
+
+const getOriginalTree=()=>originalTree
+
+
+const getDeepCopyOf=(tree)=>getDeepCopy(tree)
+
+export default {
+  getOriginalTree,
+  getDeepCopyOf
+};
